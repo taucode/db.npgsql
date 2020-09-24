d841f55edead37cf6752c0e20a2f68140df1e0e5
Andrey Kovalenko <ak@taucode.com>
2020-09-24 17:50:18 +0300

    Minimal codebse for dev to get CI'ed.

---
+++ test/TauCode.Db.Npgsql.Tests/TodoTempTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Db.Npgsql.Tests
+{
+    [TestFixture]
+    public class TodoTempTests
+    {
+        [Test]
+        public void TodoTempTestDummy()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Pass("Added for CI init. Todo: remove asap.");
+        }
+    }
+}
