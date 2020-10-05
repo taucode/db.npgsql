443a1ad2497583bda7f325f4be2496dc6596777d
Andrey Kovalenko <ak@taucode.com>
2020-10-05 19:59:54 +0300

    Refactored factory.

--- src/TauCode.Db.Npgsql/NpgsqlUtilityFactory.cs
+++ src/TauCode.Db.Npgsql/NpgsqlUtilityFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Npgsql;
 using System.Data;
 
 namespace TauCode.Db.Npgsql
@@ -14,16 +14,15 @@ namespace TauCode.Db.Npgsql
         public IDbDialect GetDialect() => NpgsqlDialect.Instance;
 
         public IDbScriptBuilder CreateScriptBuilder(string schema) => new NpgsqlScriptBuilder(schema);
+        public IDbConnection CreateConnection() => new NpgsqlConnection();
 
-        public IDbInspector CreateDbInspector(IDbConnection connection, string schema) => new NpgsqlInspector(connection, schema);
+        public IDbInspector CreateInspector(IDbConnection connection, string schema) => new NpgsqlInspector(connection, schema);
 
         public IDbTableInspector CreateTableInspector(IDbConnection connection, string schema, string tableName) =>
             new NpgsqlTableInspector(connection, schema, tableName);
 
         public IDbCruder CreateCruder(IDbConnection connection, string schema) => new NpgsqlCruder(connection, schema);
 
-        public IDbSerializer CreateDbSerializer(IDbConnection connection, string schema) => new NpgsqlSerializer(connection, schema);
-
-        public IDbConverter CreateDbConverter() => throw new NotSupportedException();
+        public IDbSerializer CreateSerializer(IDbConnection connection, string schema) => new NpgsqlSerializer(connection, schema);
     }
 }
