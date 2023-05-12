e4636987142269b61601ddf08f9dce83f91b02e8
Andrey Kovalenko <ak@taucode.com>
2023-05-12 16:25:53 +0300

    Refactoring for Econera.Money.

---
+++ src/TauCode.Db.Npgsql/NpgsqlInstructionProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+
+namespace TauCode.Db.Npgsql;
+
+public class NpgsqlInstructionProcessor : InstructionProcessor
+{
+    #region ctor
+
+    public NpgsqlInstructionProcessor()
+    {
+
+    }
+
+    public NpgsqlInstructionProcessor(NpgsqlConnection connection)
+        : base(connection)
+    {
+
+    }
+
+    #endregion
+
+    #region Overridden
+
+    public override IUtilityFactory Factory => NpgsqlUtilityFactory.Instance;
+
+    #endregion
+}
